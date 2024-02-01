852783888b200cb0776aee82404a582646b96b85
Volodymyr Kryvytskyi <VolodymyrKryvytskyi@my.uopeople.edu>
2024-02-01 19:28:21 +0900

    Initial Commit

---
+++ IAmRich/IAmRich.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRich.swift
+//  I Am Rich
+//
+//  Created by Volodymyr Kryvytskyi on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
